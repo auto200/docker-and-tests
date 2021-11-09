697a9175688f03107299189a42f10ab89ddaee0e
auto200 <michal.warac@gmail.com>
2021-11-09 12:45:57 +0100

    Fix typo

--- src/lib/trzmielar/trzmielar.test.ts
+++ src/lib/trzmielar/trzmielar.test.ts
@@ -1,19 +1,19 @@
 import { trzmielar } from "./trzmielar";
 
 describe("trzmielar", () => {
-  test("produces signle digit number", () => {
+  test("produces single digit number", () => {
     expect(trzmielar(123)).toBe(6);
   });
 
-  test("produces signle digit number from multiple digit number", () => {
+  test("produces single digit number from multiple digit number", () => {
     expect(trzmielar(123602105)).toBe(2);
   });
 
-  test("produces signle digit number from multiple digit negative number", () => {
+  test("produces single digit number from multiple digit negative number", () => {
     expect(trzmielar(-123602105)).toBe(-2);
   });
 
-  test("produces signle digit number from 0", () => {
+  test("produces single digit number from 0", () => {
     expect(trzmielar(0)).toBe(0);
   });
 });
